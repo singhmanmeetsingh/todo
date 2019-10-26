eccade30a0c7e373b84c7dd54268e74cde81de47
singhmanmeetsingh <singhmanmeet523@gmail.com>
2019-10-26 22:19:48 +0530

    added backend

---
+++ mern-todo-app/src/components/create-todo.component.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateTodo extends Component {
+
+    constructor(props) {
+        super(props);
+
+          this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+
+
+      onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+    }
+
+  onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+      onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+
+
+        onSubmit(e) {
+        e.preventDefault();
+
+        console.log(`Form submitted:`);
+
+    const newTodo = {
+          todo_description: this.state.todo_description,
+          todo_responsible: this.state.todo_responsible,
+          todo_priority: this.state.todo_priority,
+          todo_completed: this.state.todo_completed
+        };
+
+        axios.post('http://localhost:4000/todos/add', newTodo)
+          .then(res => console.log(res.data));
+
+        this.setState({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+    }
+
+    render() {
+        return (
+            <div style={{marginTop: 10}}>
+                      <h3>Create New Todo</h3>
+                  <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                          <label>Description: </label>
+                          <input  type="text"
+                                className="form-control"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                                />
+                    </div>
+                              <div >
+                        <label>Responsible: </label>
+                          <input
+                                type="text"
+                                  className="form-control"
+                                value={this.state.todo_responsible}
+                                onChange={this.onChangeTodoResponsible}
+                                />
+                    </div>
+    <div className="form-group">
+                        <div >
+                            <input
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityLow"
+                                    value="Low"
+                                    checked={this.state.todo_priority==='Low'}
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label >Low</label>
+                        </div>
+                              <div >
+                            <input
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityMedium"
+                                    value="Medium"
+                                    checked={this.state.todo_priority==='Medium'}
+                                    onChange={this.onChangeTodoPriority}
+                                      />
+                            <label >Medium</label>
+                        </div>
+                        <div >
+                            <input
+                                    type="radio"
+                                    name="priorityOptions"
+                                    id="priorityHigh"
+                                    value="High"
+                                    checked={this.state.todo_priority==='High'}
+                                    onChange={this.onChangeTodoPriority}
+                                    />
+                            <label >High</label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
